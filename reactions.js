ce6876f5c65540b14732bb0b011b4b351dc63c3d
overkam <44203987+overkam@users.noreply.github.com>
2018-10-25 22:07:31 +0300

    fixed tabs, added js file

---
+++ reactions.js
@@ -0,0 +1,15 @@
+class Reactions {
+constructor(){
+  var d = document.createElement('div');
+  d.className='button';
+  document.body.appendChild(d);
+  this.createUi();
+  }
+
+  createUi(){
+    console.log(2);
+  }
+}
+
+
+var blocktext = new Reactions();
