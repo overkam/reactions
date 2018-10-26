787ee59ae9bd22520dc045d2186a005f4c0b8c0b
overkam <44203987+overkam@users.noreply.github.com>
2018-10-26 20:01:54 +0300

    added emoji button

--- reactions.js
+++ reactions.js
@@ -1,4 +1,17 @@
-var block = document.createElement('div');
-block.classList.add('button');
-document.body.appendChild(block);
-console.log(block);
+var block = document.querySelector('.block');
+var blocktext = document.createElement('div');
+var blockemoji = document.createElement('div');
+var blockbutton = document.createElement('div');
+var button = document.createElement('div');
+
+button.classList.add('button');
+blocktext.classList.add('blocktext');
+blockemoji.classList.add('blockemoji');
+blockbutton.classList.add('blockbutton');
+
+block.appendChild(blocktext);
+block.appendChild(blockemoji);
+blockemoji.appendChild(blockbutton);
+blockbutton.appendChild(button);
+
+button.classList.toggle('buttonfocus');
