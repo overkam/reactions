3669ab707a7b4469e8946697ad94db2f48be35be
overkam <44203987+overkam@users.noreply.github.com>
2018-10-27 17:23:22 +0300

    added counter

--- reactions.js
+++ reactions.js
@@ -3,15 +3,35 @@ var blocktext = document.createElement('div');
 var blockemoji = document.createElement('div');
 var blockbutton = document.createElement('div');
 var button = document.createElement('div');
+var blockcount = document.createElement('div');
 
 button.classList.add('button');
 blocktext.classList.add('blocktext');
 blockemoji.classList.add('blockemoji');
 blockbutton.classList.add('blockbutton');
+blockcount.classList.add('blockcount');
 
 block.appendChild(blocktext);
 block.appendChild(blockemoji);
 blockemoji.appendChild(blockbutton);
+blockemoji.appendChild(blockcount);
 blockbutton.appendChild(button);
 
-button.addEventListener('click', function () { button.classList.toggle('buttonfocus'); });
+var count = 0;
+var mark = 1;
+
+button.addEventListener('click', function () {
+											if (mark % 2 != 0 ) {
+											 button.classList.toggle('buttonfocus'); 
+											 ++count;
+											 ++mark;
+											 console.log(count);
+											 console.log(mark);
+											 } else {
+											 	--count;
+											 	--mark;
+											 	console.log(count);
+											 	console.log(mark);
+											 }
+											}
+);
