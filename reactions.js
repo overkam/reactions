989bcf280048aa0d239c9e3b3230684209664dbd
overkam <44203987+overkam@users.noreply.github.com>
2018-10-31 21:37:31 +0300

    trying to usse js class

--- reactions.js
+++ reactions.js
@@ -1,5 +1,5 @@
 var block = document.querySelector('.reactions');
-var blocktext = document.createElement('div');
+/*var blocktext = document.createElement('div');
 var blockemoji = document.createElement('div');
 var blockbutton = document.createElement('div');
 var button = document.createElement('div');
@@ -17,9 +17,68 @@ blockemoji.appendChild(blockbutton);
 blockemoji.appendChild(blockcount);
 blockbutton.appendChild(button);
 
+blocktext.innerHTML = 'How do you like this article?';
+
 var count = 0;
 
-blockbutton.addEventListener('click', countReactions );
+blockbutton.addEventListener('click', countReactions );*/
+class Reactions {
+ constructor(settings) {
+  this.title = settings.title;
+  this.wrapper = document.querySelector(settings.wrapperClass);
+  this.appendToWrapper(); 
+ }
+
+ setTitle() {
+  let title = document.createElement('div');
+  title.innerHTML = this.title;
+  title.classList.add('blocktext');
+  return title;
+ }
+
+ createReaction() {
+  let div = document.createElement('div');
+  div.classList.add('button');
+  return div;
+ }
+
+ appendToWrapper() {
+  let reaction = this.createReaction();
+  let title = this.setTitle();
+  console.log(title);
+  this.wrapper.appendChild(title);
+  this.wrapper.appendChild(reaction);
+ }
+}
+
+let reaction = new Reactions({title : "Как вам статья?", wrapperClass : ".block"});
+
+/*
+class Reactions {
+ constructor(name,cssclass, first, second) {
+  this.name = name;
+  this.cssclass = cssclass;
+  this.first = first;
+  this.second = second;
+  this.createReaction(name, cssclass, first, second);
+ }
+
+ createReaction(name, cssclass, first, second) {
+  this.name = document.createElement('div');
+  this.name.classList.add(this.cssclass);
+  if (this.first != false) {
+  	this.name.appendChild(this.first);
+  }
+  if (this.second != false) {
+  	this.name.appendChild(this.second);
+  }
+  console.log(this.name);
+ }
+}
+
+var button = new Reactions('button', 'reactions__button', false, false);
+var blockbutton = new Reactions('blockbutton', 'reactions__button-wrapper', button, false);
+
 
 function countReactions(){
 	if (blockbutton.classList.contains('reactions__elem--active')) {
@@ -31,4 +90,21 @@ function countReactions(){
 	}
 	blockcount.innerHTML = count;
 };
-blocktext.innerHTML = 'How do you like this article?';
+
+
+/*class Reactions{
+	constructor (name, cssclass, child){
+		this.name = name;
+		this.cssclass = cssclass;
+		this.child = child;
+		this.createReaction(name, cssclass, child);
+	}
+	createReaction(name, cssclass, child){
+		this.name = document.createElement('div');
+		this.name.classList.add(this.cssclass)
+		document.body.appendChild(this.child);
+	}
+}
+
+var blockemoji = new Reactions (blockemoji, 'reactions__emoji', blockbutton);
+var blockbutton = new Reactions (blockbutton, 'reactions__button-wrapper', button);*/
